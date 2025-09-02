e304e6dc886670dd6a1320d7edfc59880fe09ba7
Rich Chiodo false <rchiodo@microsoft.com>
2025-09-02 14:53:08 -0700

    Add missing copyright

--- pyrefly/lib/test/tsp/tsp_interaction/get_supported_protocol_version.rs
+++ pyrefly/lib/test/tsp/tsp_interaction/get_supported_protocol_version.rs
@@ -1,4 +1,10 @@
 /*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ *
  * TSP interaction tests for getSupportedProtocolVersion request handler
  *
  * These tests verify the full TSP message protocol for getSupportedProtocolVersion requests by:
