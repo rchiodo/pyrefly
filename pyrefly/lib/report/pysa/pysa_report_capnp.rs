324da96db57b197e386c122d0ac4a8a60f7aad8e
Conner Nilsen <connernilsen@meta.com>
2026-04-15 22:26:06 -0700

    Fix headers in Pysa capnpc generator (rust)
    Reviewed By: tianhan0
    
    Differential Revision: D101032163
    
    fbshipit-source-id: c7a945bc7cc82a3fa8f43e6bfd891ee65286f303

--- pyrefly/lib/report/pysa/pysa_report_capnp.rs
+++ pyrefly/lib/report/pysa/pysa_report_capnp.rs
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 // @generated
 // @codegen-command: cd ~/fbsource/fbcode/pyrefly && ./facebook/generate_pysa_report_capnp.sh
 // @codegen-source: fbcode/pyrefly/pyrefly/lib/report/pysa/pysa_report.capnp
