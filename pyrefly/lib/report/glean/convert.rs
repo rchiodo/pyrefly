a62206fccf6e154723b92043b79a8275ffe858cb
Rubmary Rojas <rubmary@meta.com>
2026-04-27 20:32:23 -0700

    Fix missing Glean xrefs for type annotations on attribute targets
    Summary:
    Previously, type annotations on attribute assignment targets (e.g. `self.x: NodeUser = ...`) did not generate xrefs because annotation processing was coupled to variable declaration creation inside `variable_facts`, which only handled `ExprName` targets.
    
    This diff moves annotation processing for `AnnAssign` statements into `process_statement`, so xrefs are generated regardless of the assignment target type.
    
    This issue is affecting deadcode mode because missing these xrefs is causing the creation of wrong diffs like: D101292656
    
    Differential Revision: D102614645
    
    fbshipit-source-id: 4c8ce022d62e21d2b56894f04d529b41da4477ab

--- pyrefly/lib/report/glean/convert.rs
+++ pyrefly/lib/report/glean/convert.rs
@@ -255,7 +255,7 @@ struct GleanState<'a> {
 
 struct AssignInfo<'a> {
     range: TextRange,
-    annotation: Option<&'a Expr>,
+    type_info: Option<python::TypeInfo>,
     value: Option<&'a Expr>,
 }
 
@@ -1199,8 +1199,13 @@ impl GleanState<'_> {
             let fqname = self.make_fq_name_for_declaration(&name_id, &ctx.container, scope_type);
             let docstring_range =
                 next.and_then(|stmt| Docstring::range_from_stmts(slice::from_ref(stmt)));
-            let type_info = self.visit_annotation_exprs(info.annotation, &ctx.container);
-            def_infos.push(self.variable_info(fqname, info.range, type_info, docstring_range, ctx));
+            def_infos.push(self.variable_info(
+                fqname,
+                info.range,
+                info.type_info.clone(),
+                docstring_range,
+                ctx,
+            ));
 
             if name.id == dunder::ALL
                 && let Some(Expr::List(list_expr)) = info.value
@@ -1592,7 +1597,7 @@ impl GleanState<'_> {
             Stmt::Assign(assign) => {
                 let info = AssignInfo {
                     range: assign.range(),
-                    annotation: None,
+                    type_info: None,
                     value: Some(assign.value.as_ref()),
                 };
                 assign.targets.visit(&mut |target| {
@@ -1602,9 +1607,10 @@ impl GleanState<'_> {
                 self.visit_exprs(&assign.value, container);
             }
             Stmt::AnnAssign(assign) => {
+                let type_info = self.visit_annotation_exprs(Some(&assign.annotation), container);
                 let info = AssignInfo {
                     range: assign.range(),
-                    annotation: Some(&assign.annotation),
+                    type_info,
                     value: assign.value.as_ref().map(|v| v.as_ref()),
                 };
                 self.variable_facts(&assign.target, &info, context, next, &mut decl_infos);
@@ -1614,7 +1620,7 @@ impl GleanState<'_> {
             Stmt::AugAssign(assign) => {
                 let info = AssignInfo {
                     range: assign.range(),
-                    annotation: None,
+                    type_info: None,
                     value: Some(assign.value.as_ref()),
                 };
                 self.variable_facts(&assign.target, &info, context, next, &mut decl_infos);
@@ -1633,7 +1639,7 @@ impl GleanState<'_> {
                 let range = TextRange::new(stmt_for.range().start(), stmt_for.iter.range().end());
                 let info = AssignInfo {
                     range,
-                    annotation: None,
+                    type_info: None,
                     value: None,
                 };
                 stmt_for.target.visit(&mut |target| {
@@ -1654,7 +1660,7 @@ impl GleanState<'_> {
                     item.optional_vars.visit(&mut |target| {
                         let info = AssignInfo {
                             range: target.range(),
-                            annotation: None,
+                            type_info: None,
                             value: None,
                         };
                         self.variable_facts(target, &info, context, next, &mut decl_infos)
