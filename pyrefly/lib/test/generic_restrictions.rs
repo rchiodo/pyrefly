8160dd67155acce8359e0de6948ba4faca753e9b
Rebecca Chen <rechen@meta.com>
2026-06-03 16:21:43 -0700

    Add a bug= test for a binop involving a constrained TypeVar
    Summary:
    For https://github.com/facebook/pyrefly/pull/3242.
    
     ---
    
    Context:
    
    Pyrefly has overly permissive type-checking for constrained TypeVars, leading to it accepting obviously incorrect code like:
    ```
    def f() -> int | str: ...
    def g[T: (int, str)](x: T) -> T:
        return f() # ???
    ```
    The cause is this questionable rule in `is_subset_eq`, which I want to remove: https://github.com/facebook/pyrefly/blob/d345c3b42a8224a939d420b193acfb2bd402b41b/pyrefly/lib/solver/subset.rs#L1734. Unfortunately, we depend on this rule in a fair number of places, so we need to remove those dependencies first. This stack:
    
    1. Makes some tweaks to literal promotion that both improve readability and make the next step easier.
    1. Changes how we handle defaults for function parameters that include typevars in their annotations (example: `(x: T = 0) -> T`). Previously, we depended on the questionable subset rule above to partially support such defaults. This part of the stack also fixes several user-reported bugs, because we now handle this in a more principled way.
    1. Changes how we handle binary operations between constrained TypeVars. Previously, we modeled these as returning a union of their constraints. Now we detect when we preserve the original TypeVar. Again, this fixes some bugs. **<--YOU ARE HERE**
    1. ...will need to unwind more dependencies as I discover them, stay tuned =P
    
    Reviewed By: yangdanny97
    
    Differential Revision: D107199725
    
    fbshipit-source-id: 4c11e7278167374bf850572859f30ece4bb7decc

--- pyrefly/lib/test/generic_restrictions.rs
+++ pyrefly/lib/test/generic_restrictions.rs
@@ -633,6 +633,20 @@ def add2[T: int | float](x: T, y: T) -> T:
 );
 
 testcase!(
+    bug = "Spurious errors",
+    test_multiple_binops_with_constrained_typevar,
+    r#"
+from typing import TypeVar
+
+T = TypeVar("T", str, int)
+
+def foo(a: T) -> T:
+    doubled = 2 * a
+    return a + doubled  # E: `+` is not supported between `str` and `int`  # E: `+` is not supported between `int` and `str`
+    "#,
+);
+
+testcase!(
     test_multiple_args_upper_bound,
     r#"
 from typing import assert_type
